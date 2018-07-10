8da15af44b8e323c8922c582098edfd8f78ea0f9
BaleLin <bale.lin@oocl.com>
2018-07-10 13:24:24 +0800

    test 1

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,47 @@
 package practice02;
 
 public class Student {
+    String name;
+    int age;
+    int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, int klass) {
+
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+    }
+    public String introduce(){
+
+    }
 }
