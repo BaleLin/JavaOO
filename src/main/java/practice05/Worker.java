573b36421228453f787fc76f1a6a52e06e2de3b5
BaleLin <bale.lin@oocl.com>
2018-07-10 14:03:09 +0800

    test 5

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,28 @@
 package practice05;
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name,age);
 
-public class Worker {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String introduce(){
+        return super.introduce()+" I am a Worker. I have a job.";
+
+    }
 }
