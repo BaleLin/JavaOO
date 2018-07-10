13daed2fe8b33397578a6149ed9dcf44a857ce85
BaleLin <bale.lin@oocl.com>
2018-07-10 14:08:08 +0800

    test 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,18 @@
 package practice06;
 
 public class Teacher {
+    int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(int klass) {
+
+        this.klass = klass;
+    }
 }
